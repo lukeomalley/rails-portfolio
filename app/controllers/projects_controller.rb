b0d722d3bad0cc50309295072d5f498850a373c2
Luke OMalley <Lukeomalley95@gmail.com>
2019-05-10 23:12:36 -0400

    Integrated new and create actions on projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,23 @@
+# frozen_string_literal: true
+
 class ProjectsController < ApplicationController
   def index
     @project_items = Project.all
   end
 
+  def new
+    @project_item = Project.new
+  end
 
+  def create
+    @project_item = Project.new(params.require(:project).permit(:title, :subtitle, :body))
 
+    respond_to do |format|
+      if @project_item.save
+        format.html { redirect_to projects_path, notice: 'Your project is now live' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
