91b73bd3fe0a0458e3730d40af1b30ea2df64b19
Luke OMalley <Lukeomalley95@gmail.com>
2019-05-12 18:14:43 -0400

    added draft and published mode

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show edit update destroy toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -55,6 +55,15 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
