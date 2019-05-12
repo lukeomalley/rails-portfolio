85f6d38157b3fb66c4fbf4bb41ed9ebb607c7d1d
Luke OMalley <Lukeomalley95@gmail.com>
2019-05-12 17:41:43 -0400

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Blog < ApplicationRecord
+  # setup for draft and pulbish modes.  From here on we can reference them as "draft" and "publish" rather than 0 and 1
+  enum status: { draft: 0, published: 1 }
+
+  # setup for friendly id slug urls
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
